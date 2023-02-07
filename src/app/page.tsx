99e12ef761d5fafc78d049f2ce5df5fe82aa17ac
sirdan <danielsdfullerton@gmail.com>
2023-02-07 16:08:12 +0000

    dev branch init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Inter } from '@next/font/google'
 import './page.scss'
 import { db } from '@/firebase/firebase'
-import { collection, documentId, getDoc, getDocs } from 'firebase/firestore'
+import { addDoc, collection, documentId, getDoc, getDocs } from 'firebase/firestore'
+import { Page } from '@/lib/interfaces'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,24 +13,19 @@ export default async function Home() {
 
   console.log("data is: " + data.docs[0].data().pageStructure[0].containedArray[0].type)
 
+  async function addPage(pageData: Page) {
+    try {
+      const docRef = await addDoc(collection(db, 'Structure/page-layout/pages'), {
+        pageData
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
     <main className="main">
-      <div className="description">
-        <p>
-          Get started by editing&nbsp;
-          <code className="code">src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <h1>DANI</h1>
-          </a>
-        </div>
-      </div>
 
       <div className="center">
         <div className="thirteen">
@@ -37,47 +33,6 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="grid">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="className">
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className="className">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="className">
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className="className">Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="className">
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className="className">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
---
+++ src/lib/interfaces.ts
@@ -0,0 +1,9 @@
+interface Page{
+  name: string,
+  ageStructure: any[],
+  slug: string,
+}
+
+export type{
+  Page,
+}
