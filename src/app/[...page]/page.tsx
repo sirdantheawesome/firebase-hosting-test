f0dc64d22d8648d7d5aed6c151a224e1f0f272c4
sirdan <danielsdfullerton@gmail.com>
2023-02-12 21:06:06 +0000

    db queries

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,20 +1,32 @@
 import './page.scss'
-import { collection, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from '@/firebase/firebase';
 
 const Page = async (params: { params: { page: string }, searchParams: any }) => {
 
   const slugData = params.params.page
-  const slug = slugData.toString().replaceAll(",", "/")
+  const slug = "/" + slugData.toString().replaceAll(",", "/")
 
-  const pagesRef = await collection(db, "pages");
-  const q = await query(pagesRef, where("slug", "==", slug));
+  const q = query(
+    collection(db, "/Structure/page-layout/pages"),
+    where("slug", "==", slug)
+  );
+  const page = await getDocs(q);
 
-  console.log(q)
+  console.log(page.size ? page.docs[0].data() : '')
 
   return (
     <div className='main-content dev-mode'>
-      <h1>slug: {slug}</h1>
+      <h1>From local:</h1>
+      <h2>slug: {slug}</h2>
+      <h1>From database:</h1>
+      {page.size ?
+        <>
+          <h2>slug: {page.docs[0].data().slug}</h2>
+          <h2>name: {page.docs[0].data().name}</h2>
+        </>
+        : <h2>Error: No page data found</h2>}
+
     </div>
   );
 };
